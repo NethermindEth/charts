03f8c362340d9015a1adcf69031a0dfce8935bcb
AntiD2ta <mtpotro41@gmail.com>
2024-01-24 13:02:03 +0100

    Fix linter reported issues

--- scripts/check-codeowners.sh
+++ scripts/check-codeowners.sh
@@ -13,10 +13,10 @@ cat <<EOF
 
 EOF
 
-for DIR in $(ls -1 -d ./charts/*)
+for DIR in ./charts/*
 do
   FILE="$DIR/Chart.yaml"
-  DIR=$(echo $DIR | sed 's/^\.//')
-  MAINTAINERS=$(yq e '.maintainers.[].name' $FILE| sed 's/^/@/' | sort --ignore-case)
-  echo $DIR/ $MAINTAINERS
+  DIR=$(echo "$DIR" | sed 's/^\.//')
+  MAINTAINERS=$(yq e '.maintainers.[].name' "$FILE"| sed 's/^/@/' | sort --ignore-case)
+  echo "$DIR"/ $MAINTAINERS
 done
